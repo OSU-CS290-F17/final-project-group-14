09ad6a94b346a0bea2ccbf0b2d96248b524ee45c
Jakob Nelson <nelsjako@oregonstate.edu>
2017-11-29 16:50:16 -0800

    started a basic server.js file and made the package-lock.json file

---
+++ server.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var port = process.env.PORT || 3000;
+
+/* These get requests will need some more work as we get further on
+ * there is nothing being passed in to generate dynamic content like in the accoutPage
+ * We will also need to decide what we want the data in our database to look like
+ * before I can really move forward with some of this stuff
+ */
+
+// root path
+app.get('/', function (req, res) {
+  res.status(200).render('homePage');
+});
+
+// signIn page
+app.get('/signIn', function (req, res) {
+  res.status(200).render('signIn');
+});
+
+// accountPage
+app.get('/accountPage', function (req, res) {
+  res.status(200).render('accountPage');
+});
+
+// 404 page
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+});
+
+app.use(express.static('public'));
+
+app.listen(port, funtion () {
+  console.log("== Server is listening on port", port);
+});
