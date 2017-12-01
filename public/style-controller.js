11e7cca307a8daa01d3fb0971b25314910dac253
Mason Sidebottom <mason.sidebottom@me.com>
2017-12-01 11:19:26 -0800

    Carousel Work
    Icons added, better resizing

--- public/style-controller.js
+++ public/style-controller.js
@@ -7,50 +7,63 @@ Notes for integration
 
 
 */
-var carousel = undefined;
-var allActive = 5;
-var isREADY = false;
+
+
+var navState = 0;
 document.addEventListener("DOMContentLoaded", function(){
-  isREADY = true;
-  carousel = document.getElementById('main-carousel');
+  document.getElementById('main-carousel');
   spaceCarousel();
-  carousel.addEventListener('resize', function(){
-    spaceCarousel();
-  })
+  window.addEventListener('resize', function(){
+    if(!activeSizeSafe()) fixActives();
+  });
+
+  window.addEventListener('scroll', function(){
+      switchNavBar(scrollY);
+    });
+
 });
 
+function switchNavBar(loc) {
+  if(navState == 0 && loc > 360){
+    document.getElementById('navbar').classList.add('nav-dark')
+    navState = 1;
+  }
+  else if(navState == 1 && loc < 361){
+    document.getElementById('navbar').classList.remove('nav-dark')
+    navState = 0;
+  }
+}
 
 
 function spaceCarousel(){
   var active = document.getElementsByClassName('active');
-  var cStyle = window.getComputedStyle(carousel);
-  // Sets width to carouselWidth/numElems
-  var carW = cStyle.width.substring(0, cStyle.width.length-2)
-  console.log(carW);
-
-  var width = carW/active.length;
-  console.log(width);
-  console.log()
-  if(width < 180){
-    var max = Math.floor(carW/180);
-    console.log("max ", max);
-    allActive = max;
-    adjustActives(0)
-    active = document.getElementsByClassName('active');
-    width = carW/active.length;
-  }
+  var size = 100/active.length;
   for(var i = 0; i < active.length; i++){
-    active[i].style.width = width + "px";
+    active[i].style.width = size + "%";
   }
 }
 
-function adjustActives(start){
-  var items = document.getElementsByClassName("menu-item")
-  for(var i = start; i < items.length ; i++){
-    if( i < allActive){
-      items[i].classList.add('active');
+function fixActives(){
+  var active = document.getElementsByClassName('active');
+  var total = getActiveWidth() * active.length;
+  var max = Math.floor(total / 170);
+  var amount = (max < 4) ? 3 : 5;
+  var allItems = document.getElementsByClassName('menu-item');
+  for(var i = 0; i < allItems.length; i++){
+    if(i < amount){
+      allItems[i].classList.add('active');
     }else{
-      items[i].classList.remove('active');
+      allItems[i].classList.remove('active');
     }
   }
+  spaceCarousel();
+}
+
+function activeSizeSafe(){
+  var w = getActiveWidth();
+  return (w > 170 && w < 250) ? true : false;
+}
+
+function getActiveWidth(){
+  return window.getComputedStyle(document.getElementsByClassName('active')[0]).width.replace("px", "")
 }
