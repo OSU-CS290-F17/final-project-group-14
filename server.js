dbc43aa2996a0be240dee5beacd1a2489df0b21a
Jakob Nelson <nelsjako@oregonstate.edu>
2017-12-02 14:44:30 -0800

    staeted on modifying server to handle signUp

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
+  '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+var mongoConnection = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -16,8 +29,17 @@ var port = process.env.PORT || 3000;
  */
 
 // root path
-app.get('/', function (req, res) {
-  res.status(200).render('homePage');
+app.get('/:username/accountPage', function (req, res) {
+  dataCollection.find({ username: req.params.username }).toArray(funtion (err, results) {
+    if (err){
+      res.status(500).send("Error fetching account data");
+    }else if (results > 0) {
+      res.status(200).render('accountPage', {
+        account: results
+      });
+    }
+  })
+  res.status(200).render('accountPage');
 });
 
 // signIn page
@@ -35,13 +57,33 @@ app.get('/accountPage', function (req, res) {
   res.status(200).render('accountPage');
 });
 
+app.post('/signUp/addAccount', function (req, res) {
+
+  if (req.body && req.body.username) {
+    var accountObj = {
+      username: req.body.username,
+
+    };
+
+    dataCollection.insert( accountObj );
+  } else {
+    res.status(400).send("Request body needs a 'username' field");
+  }
+});
+
 app.use(express.static('public'));
 
 // 404 page
 app.get('*', function (req, res) {
   res.status(404).render('404');
 });
-
-app.listen(port, function () {
-  console.log("== Server is listening on port", port);
-});
+MongoClient.connect(mongoURL, function (err, connection) {
+  if (err) {
+    throw err;
+  }
+  mongoConnection = connection;
+  var dataCollection = mongoConnection.collection()
+  app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+  });
+})
