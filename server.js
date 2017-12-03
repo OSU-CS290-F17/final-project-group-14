8446d93982eb7dd139d943d5f059fd61c797e71d
wasamiam <wasamiam@gmail.com>
2017-12-02 17:33:34 -0800

    Added server connection

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
+
+var mongoURL = "mongodb://cs290_wilsosam:cs290_wilsosam@classmongo.engr.oregonstate.edu:27017/cs290_wilsosam";
+var mongoConnection = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -42,6 +46,12 @@ app.get('*', function (req, res) {
   res.status(404).render('404');
 });
 
-app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+MongoClient.connect(mongoURL, function (err, connection) {
+  if (err) {
+    throw err;
+  }
+  mongoConnection = connection;
+  app.listen(port, function () {
+    console.log("== Server listening on port:", port);
+  });
 });
